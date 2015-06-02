12bf193712a8b92f1b32fdf99ddee87b3afa1f74
yenyuyu <billy78116@gmail.com>
2015-06-02 21:10:14 +0800

    multi snake

---
+++ test.m
@@ -0,0 +1,14 @@
+aiSnakeposTab = zeros(8,2,5);
+aiSnakeposTab(:,1,1) = 3; 
+aiSnakeposTab(:,2,1) =  18:1:25;
+aiSnakeposTab(:,1,2) = [12,11,10,9,9,10,11,12]; 
+aiSnakeposTab(:,2,2) =  [3,3,3,3,2,2,2,2];
+aiSnakeposTab(:,1,3) = [18,17,16,15,15,16,17,18]; 
+aiSnakeposTab(:,2,3) =  [3,3,3,3,2,2,2,2];
+aiSnakeposTab(:,1,4) = [15,14,13,12,12,13,14,15]; 
+aiSnakeposTab(:,2,4) =  [26,26,26,26,27,27,27,27];
+aiSnakeposTab(:,1,5) = 27; 
+aiSnakeposTab(:,2,5) = 24:-1:17;
+
+aiSnakeDirTab = {'left'; 'right';'right';'left';'right'};
+
